d6e29a949724b31d732b2e801e841557e61b03d3
purvitak <purvitakpari@gmail.com>
2026-04-18 21:19:09 +0530

    updated
    Signed-off-by: purvitak <purvitakpari@gmail.com>

--- ultralytics/nn/modules/bifpn.py
+++ ultralytics/nn/modules/bifpn.py
@@ -8,27 +8,39 @@ class BiFPN_Add(nn.Module):
     """
     BiFPN weighted feature fusion node.
     Receives list of feature maps from YAML multi-input syntax.
-    All inputs upsampled/downsampled to match first input spatial size.
-    Output channels = c2.
+    Handles different channel sizes by projecting them to match c2.
     """
     def __init__(self, c1, c2):
         super().__init__()
+        self.c2 = c2
         self.w = nn.Parameter(torch.ones(2, dtype=torch.float32))
         self.relu = nn.ReLU()
-        self.conv = Conv(c1, c2, 1, 1)
         self.eps = 1e-4
+        self.conv = Conv(c1, c2, 1, 1)
 
     def forward(self, x):
-        # x is list of tensors from multiple layers
+        # Project inputs to have matching channels if needed
+        projected = []
+        for xi in x:
+            if xi.shape[1] != self.c2:
+                temp_conv = Conv(xi.shape[1], self.c2, 1, 1).to(xi.device)
+                xi = temp_conv(xi)
+            projected.append(xi)
+        
+        # Weighted fusion
         w = self.relu(self.w)
         w = w / (w.sum() + self.eps)
+        
         # Resize all to first tensor's spatial size
-        h, ww = x[0].shape[2:]
-        out = sum(
-            w[i] * F.interpolate(
-                xi, size=(h, ww),
-                mode="bilinear", align_corners=False
+        h, ww = projected[0].shape[2:]
+        out = w[0] * projected[0]
+        for i in range(1, min(2, len(projected))):
+            resized = F.interpolate(
+                projected[i], 
+                size=(h, ww),
+                mode="bilinear", 
+                align_corners=False
             )
-            for i, xi in enumerate(x[:2])
-        )
+            out = out + w[i] * resized
+        
         return self.conv(out)
